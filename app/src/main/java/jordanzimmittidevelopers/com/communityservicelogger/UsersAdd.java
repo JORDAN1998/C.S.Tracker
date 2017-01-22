d8fad997cf9d35038b0534828023c6f1c5f24725
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-01-21 22:54:00 -0500

    Created New Activity UsersAdd

--- app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersAdd.java
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersAdd.java
@@ -8,6 +8,6 @@ public class UsersAdd extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_ui);
+        setContentView(R.layout.users_add_ui);
     }
 }
