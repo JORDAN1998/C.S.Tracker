82c5fd29547bf3e5c25910681b040266d331188a
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-01-22 10:52:13 -0500

    add menu in activity

--- app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersAdd.java
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersAdd.java
@@ -1,13 +1,48 @@
 package jordanzimmittidevelopers.com.communityservicelogger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class UsersAdd extends AppCompatActivity {
 
+    // What Happens When Activity Starts//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Starts UI For Activity//
         setContentView(R.layout.users_add_ui);
+    }
+
+    // Creates Menu And All Its Components//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflates The Menu / This Adds Items To The Action Bar If It Is Present//
+        getMenuInflater().inflate(R.menu.users_add_menu, menu);
+
+        // Kill Code//
+        return true;
+    }
+
+    // What Happens When Menu Buttons Are Clicked//
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Figures Out What Menu Button Was Pressed//
+        int id = item.getItemId();
+
+        // What Happens When usersAddSave Is Pressed//
+        if (id == R.id.usersAddSave) {
+
+
+            // Kill Code//
+            return true;
+        }
+
+        // Kill Code//
+        return super.onOptionsItemSelected(item);
     }
 }
