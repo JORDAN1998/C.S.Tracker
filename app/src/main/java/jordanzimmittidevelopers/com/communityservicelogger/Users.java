d536b663bd22503e8dbf5fb32b37f28ea70b88f6
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-01-21 21:41:18 -0500

    Added Comments / Cleaned Up Code

--- app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/Users.java
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/Users.java
@@ -3,11 +3,15 @@ package jordanzimmittidevelopers.com.communityservicelogger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Users Class Created By Jordan Zimmitti 1-21-17//
 public class Users extends AppCompatActivity {
 
+    // What Happens When Activity Starts//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Starts UI For Activity//
         setContentView(R.layout.users_ui);
     }
 }
