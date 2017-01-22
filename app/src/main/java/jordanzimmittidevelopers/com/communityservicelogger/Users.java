e26a3634f185b3ee98db8a1084d7104ce7305452
Jordan Zimmitti <jordanzimmitti@Jordans-MacBook-Pro.local>
2017-01-21 21:16:22 -0500

    This is the first commit

---
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/Users.java
@@ -0,0 +1,13 @@
+package jordanzimmittidevelopers.com.communityservicelogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.users_ui);
+    }
+}
