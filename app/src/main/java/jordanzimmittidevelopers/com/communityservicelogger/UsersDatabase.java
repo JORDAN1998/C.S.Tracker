8134cb176049bc487e8ec9504aee36bda7ee9199
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-01-22 11:29:31 -0500

    Created UsersDatabase Class

--- app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersDatabase.java
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersDatabase.java
@@ -2,4 +2,11 @@ package jordanzimmittidevelopers.com.communityservicelogger;
 
 // UsersDatabase Class Created By Jordan Zimmitti 1-22-17//
 public class UsersDatabase {
+
+    // Used For Logging Database Version Changes//
+    private static final String TAG = "UsersDatabase";
+
+    // Row Names//
+    public static final String KEY_ROW_ID_NUMBER = "_id";
+    public static final String KEY_NAME = "name";
 }
