a00af9655e155d8cb08541d8203a67c8dbd26420
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-01-21 22:53:18 -0500

    Created New Activity UsersAdd

--- app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersView.java
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/UsersView.java
@@ -1,10 +1,17 @@
 package jordanzimmittidevelopers.com.communityservicelogger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 // UsersView Class Created By Jordan Zimmitti 1-21-17//
 public class UsersView extends AppCompatActivity {
+
+    // Define Variable Vibrator vibe//
+    Vibrator vibe;
 
     // What Happens When Activity Starts//
     @Override
@@ -13,5 +20,16 @@ public class UsersView extends AppCompatActivity {
 
         // Starts UI For Activity//
         setContentView(R.layout.users_ui);
+
+        // Instantiate Variable Vibrator vibe//
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    // What Happens When Fab Btn Is Clicked//
+    public void onClickFab(View view) {
+
+        // Start Activity UsersAdd//
+        Intent UsersAdd = new Intent(this, UsersView.class);
+        startActivity(UsersAdd);
     }
 }
