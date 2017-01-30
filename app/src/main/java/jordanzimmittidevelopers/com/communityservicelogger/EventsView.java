1863063bf51561cc3b55970ad2b7bfd635b47f12
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-01-29 22:52:04 -0500

    Jordan Mac

---
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/EventsView.java
@@ -0,0 +1,21 @@
+package jordanzimmittidevelopers.com.communityservicelogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+// EventsView Class Created By Jordan Zimmitti 1-29-17//
+public class EventsView extends AppCompatActivity {
+
+    // What Happens When Activity Starts//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Starts UI For Activity//
+        setContentView(R.layout.events_view_ui);
+    }
+
+    public void onClickFab(View view) {
+    }
+}
