bb4d50d0213d18d12f9a892d57a0b95a60d68854
Jordan Zimmitti <jordanzimmitti@gmail.com>
2017-02-11 16:23:54 -0500

    Jordan Mac

--- app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/EventsAdd.java
+++ app/src/main/java/jordanzimmittidevelopers/com/communityservicelogger/EventsAdd.java
@@ -11,7 +11,10 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 // EventsAdd Class Created By Jordan Zimmitti 2-09-17//
 public class EventsAdd extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
@@ -19,6 +22,9 @@ public class EventsAdd extends AppCompatActivity implements DatePickerDialog.OnD
     //<editor-fold desc="Variables">
 
     //<editor-fold desc="Extra">
+
+    // Define Variable int timeTotalAdded//
+    int timeTotalAdded = 0;
 
     // Define Variable Vibrator Vibe//
     private Vibrator vibe;
@@ -105,6 +111,41 @@ public class EventsAdd extends AppCompatActivity implements DatePickerDialog.OnD
 
     }
 
+    // Method That Calculates Total Time//
+    private void calculateTimeTotal() throws ParseException {
+
+        // What Happens When militaryTimeStart And militaryTimeEnd Have Values//
+        if (militaryTimeStart != null & militaryTimeEnd != null) {
+
+            // Define Variable Date timeStartValue//
+            Date timeStartValue = null;
+
+            // Define Variable Date timeEndValue//
+            Date timeEndValue = null;
+
+            // Creates Simple Date Format//
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+
+            // Puts Military Time Start Into Time Format//
+            timeStartValue = simpleDateFormat.parse(militaryTimeStart);
+
+            // Puts Military Time Start Into Time Format//
+            timeEndValue = simpleDateFormat.parse(militaryTimeEnd);
+
+            // Subtract End Time And Start Time In Military Time And Then Puts It Into Standard Time//
+            long difference = timeEndValue.getTime() - timeStartValue.getTime();
+            int days = (int) (difference / (1000 * 60 * 60 * 24));
+            int hours = (int) ((difference - (1000 * 60 * 60 * 24 * days)) / (1000 * 60 * 60));
+            int min = (int) (difference - (1000 * 60 * 60 * 24 * days) - (1000 * 60 * 60 * hours)) / (1000 * 60);
+            timeTotalAdded = hours * 60;
+            timeTotalAdded = timeTotalAdded + min;
+            String timeDifference = (hours < 0 ? "0" + -hours : hours) + ":" + (min < 10 ? "0" + min : min);
+
+            // Set Time Difference//
+            eventsAddTimeTotal.setText(timeDifference);
+        }
+    }
+
     // Method That Instantiates Widgets//
     private void instantiateWidgets() {
 
@@ -179,11 +220,11 @@ public class EventsAdd extends AppCompatActivity implements DatePickerDialog.OnD
         //<editor-fold desc="OnTimeSetListener">
 
         // What Happens When user Picks Time//
-        timePickerDialogTimeStart.setOnTimeSetListener(new TimePickerDialog.OnTimeSetListener() {
+        timePickerDialogTimeStart.setOnTimeSetListener(new TimePickerDialog.OnTimeSetListener()  {
 
             // What Happens When Time Is Set//
             @Override
-            public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+            public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second)  {
 
                 // Define And Instantiate Variable int militaryHour//
                 int militaryHour = hourOfDay;
@@ -259,6 +300,13 @@ public class EventsAdd extends AppCompatActivity implements DatePickerDialog.OnD
 
                 // Set eventsAddTimeStart Text//
                 eventsAddTimeStart.setText(timeStartString);
+
+                try {
+
+                    // Initiate calculateTimeTotal Method//
+                    calculateTimeTotal();
+
+                } catch (ParseException e) { e.printStackTrace(); }
             }
         });
 
@@ -267,9 +315,115 @@ public class EventsAdd extends AppCompatActivity implements DatePickerDialog.OnD
 
     // What Happens When eventsAddTimeEnd Is Clicked//
     public void onClickTimeEnd(View view) {
-    }
 
-    // What Happens When eventsAddTimeTotal Is Clicked//
-    public void onClickTimeTotal(View view) {
+        // Vibrates For 50m//
+        vibe.vibrate(50);
+
+        // Define And Instantiate Variable Calendar calendar//
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+
+        // Creates New Time Picker Dialog//
+        TimePickerDialog timePickerDialogTimeEnd = TimePickerDialog.newInstance(EventsAdd.this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);
+
+        // Show datePickerDialog//
+        timePickerDialogTimeEnd.show(getFragmentManager(), "timePickerDialogTimeStart");
+
+        // Set Version Of datePickerDialog//
+        timePickerDialogTimeEnd.setVersion(TimePickerDialog.Version.VERSION_2);
+
+        //<editor-fold desc="OnTimeSetListener">
+
+        // What Happens When user Picks Time//
+        timePickerDialogTimeEnd.setOnTimeSetListener(new TimePickerDialog.OnTimeSetListener() {
+
+            // What Happens When Time Is Set//
+            @Override
+            public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+
+                // Define And Instantiate Variable int militaryHour//
+                int militaryHour = hourOfDay;
+
+                // Define Variable String amOrPm//
+                String amOrPm;
+
+                // Define Variable String min//
+                String min;
+
+                // Define Variable String timeStartString//
+                String timeEndString;
+
+                // What Happens If militaryHour Equals Zero//
+                if (militaryHour == 0) {
+
+                    // Sets hour to 24//
+                    militaryHour = 24;
+                }
+
+                // What Happens When hourOfDay Is Bigger Than Twelve//
+                if (hourOfDay > 12) {
+
+                    // Subtract Twelve//
+                    hourOfDay -= 12;
+
+                    // Set Equal To PM//
+                    amOrPm = "PM";
+
+                }
+
+                // What Happens When hourOfDay Equals Zero//
+                else if (hourOfDay == 0) {
+
+                    // Add Twelve//
+                    hourOfDay += 12;
+
+                    // Set Equal To AM//
+                    amOrPm = "AM";
+                }
+
+                // What Happens When hourOfDay Equals Twelve//
+                else if (hourOfDay == 12) {
+
+                    // Set Equal To PM//
+                    amOrPm = "PM";
+                }
+
+                // What Happens For Everything Else//
+                else {
+
+                    // Set Equal To AM//
+                    amOrPm = "AM";
+                }
+
+                // What Happens When minute Is Less Than Ten//
+                if (minute < 10) {
+
+                    // Add A zero To The Minute//
+                    min = "0" + minute;
+
+                } else {
+
+                    // Set min Equal To minute//
+                    min = String.valueOf(minute);
+                }
+
+                // Set Military Time Hour And Minute//
+                militaryTimeEnd = String.valueOf(militaryHour) + ":" + minute;
+
+                // Set Standard Time Hour And Minute//
+                timeEndString = String.valueOf(hourOfDay) + ':' + min + " " + amOrPm;
+
+                // Set eventsAddTimeStart Text//
+                eventsAddTimeEnd.setText(timeEndString);
+
+                try {
+
+                    // Initiate calculateTimeTotal Method//
+                    calculateTimeTotal();
+
+                } catch (ParseException e) { e.printStackTrace(); }
+            }
+        });
+
+        //</editor-fold>
     }
 }
